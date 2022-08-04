337366d0457b2e9140d32003505a069f5472ac05
Fabian Betancur <faanbece@gmail.com>
2022-08-04 16:17:34 -0500

    Añade aletorizador de servicios

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
+from flask import url_for
 from modulos.cursos import blue_cursos
 from pprint import pprint
+from random import sample
 
 app = Flask(__name__)
 app.register_blueprint(blue_cursos, url_prefix="/cursos")
@@ -12,7 +15,43 @@ app.register_blueprint(blue_cursos, url_prefix="/cursos")
 @app.route("/index")
 @app.route("/inicio")
 def inicio():
-    return render_template('index.html')
+    info_servicios = [
+        {
+            "url_foto": "img/storage-service.jpg",
+            "nombre":"Storage",
+            "descripcion":"Cumque eos in qui numquam. Aut aspernatur perferendis sed atque quia voluptas quisquam repellendus temporibus itaqueofficiis odit",
+        },
+        {
+            "url_foto": "img/logistics-service.jpg",
+            "nombre":"Logistics",
+            "descripcion":"Asperiores provident dolor accusamus pariatur dolore nam id audantium ut et iure incidunt molestiae dolor ipsam ducimus occaecati nisi",
+        },
+        {
+            "url_foto": "img/cargo-service.jpg",
+            "nombre":"Cargo",
+            "descripcion":"Dicta quam similique quia architecto eos nisi aut ratione aut ipsum reiciendis sit doloremque oluptatem aut et molestiae ut et nihil",
+        },
+        {
+            "url_foto": "img/trucking-service.jpg",
+            "nombre":"Trucking",
+            "descripcion":"Dicta quam similique quia architecto eos nisi aut ratione aut ipsum reiciendis sit doloremque oluptatem aut et molestiae ut et nihil",
+        },
+        {
+            "url_foto": "img/packaging-service.jpg",
+            "nombre":"Packaging",
+            "descripcion":"Illo consequuntur quisquam delectus praesentium modi dignissimos facere vel cum onsequuntur maiores beatae consequatur magni voluptates",
+        },
+        {
+            "url_foto": "img/warehousing-service.jpg",
+            "nombre":"Warehousing",
+            "descripcion":"Quas assumenda non occaecati molestiae. In aut earum sed natus eatae in vero. Ab modi quisquam aut nostrum unde et qui est non quo nulla",
+        },
+    ]
+
+    return render_template(
+        'index.html',
+        servicios=sample(info_servicios, 3)
+    )
 
 
 @app.route("/about")
