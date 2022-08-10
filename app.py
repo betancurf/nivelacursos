696e9f9fc98b74bb3a2158c4040d6ad32a089b0f
Fabian Betancur <faanbece@gmail.com>
2022-08-09 19:18:37 -0500

    Cambia libreria de pass

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import environ
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -14,9 +15,7 @@ from modelos import db
 from modelos import PQR
 from modelos import Usuario
 
-from bcrypt import gensalt
-from bcrypt import hashpw as encriptar_password
-from bcrypt import checkpw as confirmar_password
+from passlib.hash import pbkdf2_sha256 as sha256
 
 # Zona de configuración de la app principal
 app = Flask(__name__)
@@ -48,6 +47,13 @@ def user_loader(user_id):
     except:
         return 
 
+
+def encriptar_password(texto_plano: str) -> str:
+    texto_seguro = sha256.hash(texto_plano.encode("utf-8"))
+    return texto_seguro
+
+def validar_password(texto_plano:str , texto_seguro: str)-> str:
+    return sha256.verify(texto_plano.encode("utf-8"), texto_seguro)
 
 # Zona de endpoints
 
@@ -239,7 +245,7 @@ def registro_f():
         telefono = request.form.get("telefono")
 
         if nickname and correo and password:
-            pass_seguro = encriptar_password( password.encode("utf-8"), gensalt())
+            pass_seguro = encriptar_password(password)
             try:
                 nuevo_usuario = Usuario.create(
                     nickname=nickname,
@@ -270,7 +276,7 @@ def ingreso_f():
                 return f"Error, no se pudo obtener el usuario"
 
             pass_actual = usuario.password
-            hacen_match = confirmar_password(password.encode("utf-8"), pass_actual.encode("utf-8"))
+            hacen_match = validar_password(password, pass_actual)
 
             if hacen_match:
                 login_user(usuario)
