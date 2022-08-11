074e0a13d7e0571c57d23389c7df9911e5320ed3
Fabian Betancur <faanbece@gmail.com>
2022-08-10 20:33:01 -0500

    Agrega logica para crear, aceptar y denegar solicitudes para ser profesor

--- modulos/admin.py
+++ modulos/admin.py
@@ -1,18 +1,59 @@
-from flask import Blueprint
-from flask_login import login_required
 from flask import render_template
+from flask import Blueprint
+from flask import redirect
+from flask import url_for
+from flask import flash
+from flask_login import current_user
+from flask_login import login_required
+from enums import FlashCat as cat
+from modelos import SolicitudParaProfesor
+from modelos import Usuario
 
 blue_admin = Blueprint("app_admin", __name__)
 
-@blue_admin.route("/")
+
+@blue_admin.route("/solicitudes")
 @login_required
-def ver_f():
-    return render_template("admin/administrar_profesores.html")
+def solicitudes_f():
+    if not current_user.rol == Usuario.ROLES.ADMIN.value:
+        flash("Usted no debería estar aquí", cat.ALERTA.value)
+        return redirect(url_for("inicio"))
 
-@blue_admin.route("/")
-def aceptar_f():
-    return "aquí iría la misma url de admin pero con la socicitud que acepté  abajo en las editables"
+    solicitudes = SolicitudParaProfesor.select()
+    return render_template(
+        "admin/solicitudes.html",
+        solicitudes=solicitudes
+    )
 
-@blue_admin.route("/")
-def denegar_f():
-    return "aquí iría la misma url de admin pero que la socicitud que acepté desaparezca"
+@blue_admin.route("/solicitudes/aceptar/<id_solicitud>")
+@login_required
+def aceptar_solicitud_f(id_solicitud):
+    if not current_user.rol == Usuario.ROLES.ADMIN.value:
+        return "Usted no debería estar aquí"
+
+    try:
+        solicitud = SolicitudParaProfesor.get(SolicitudParaProfesor.id==id_solicitud)
+        solicitud.solicitante.rol = Usuario.ROLES.PROFESOR.value
+        solicitud.solicitante.save()
+        solicitud.delete_instance()
+        flash("Profesor aceptado con exito")
+    except:
+        flash("Solicitud no encontrada", cat.FALLA.value)
+
+    return redirect(url_for("app_admin.solicitudes_f"))
+
+@blue_admin.route("/solicitudes/denegar/<id_solicitud>")
+@login_required
+def denegar_solicitud_f(id_solicitud):
+    if not current_user.rol == Usuario.ROLES.ADMIN.value:
+        return "Usted no debería estar aquí"
+
+    try:
+        solicitud = SolicitudParaProfesor.get(SolicitudParaProfesor.id==id_solicitud)
+        # solicitud.solicitante.rol = Usuario.ROLES.PROFESOR.value
+        solicitud.delete_instance()
+        flash("Profesor denegado con exito", cat.INFO.value)
+    except:
+        flash("Solicitud no encontrada", cat.FALLA.value)
+
+    return redirect(url_for("app_admin.solicitudes_f"))
